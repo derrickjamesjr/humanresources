92f5126b5cc0fbe41746dbc440aa8b6ae284af76
Derrick James <ballyouth@gmail.com>
2022-10-08 14:24:00 -0400

    moved queries into db file

---
+++ db/queries.sql
@@ -0,0 +1,28 @@
+--Select all departments
+SELECT * FROM department;
+
+--Select from names only
+SELECT name FROM department;
+
+--Select all roles
+SELECT * FROM role;
+
+--Select all employees
+SELECT * FROM employee;
+
+--Add to a table
+INSERT INTO department (name)
+VALUES();
+
+--Add to a table
+INSERT INTO role()
+VALUES();
+
+--Add to a table
+INSERT INTO employee (name)
+VALUES();
+
+--change employee role
+UPDATE employee
+    SET role_id =
+    WHERE id =;
